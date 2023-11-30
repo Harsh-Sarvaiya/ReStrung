32b014a662e78842eb80fde6a84a9a30a9e0dbe4
ekamtaneja <95505981+ekamtaneja@users.noreply.github.com>
2023-11-29 22:14:27 -0800

    Update admin.py: added code for customers table.

--- website/playground/admin.py
+++ website/playground/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import stringers
+from .models import stringers, customers
 # Register your models here.
 
 admin.site.register(stringers)
+admin.site.register(customers)
